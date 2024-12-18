c586e12510727700593f6b13cc754392d7b0a880
BNBendi <127506138+BNBendi@users.noreply.github.com>
2024-12-18 13:52:24 +0100

    kis vált

---
+++ kat.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const genreDropdown = document.getElementById('genre-dropdown');
+    const typeDropdown = document.getElementById('type-dropdown');
+  
+    const genreSelected = genreDropdown.querySelector('.dropdown-selected');
+    const typeSelected = typeDropdown.querySelector('.dropdown-selected');
+  
+    // Dropdown menük megnyitása
+    [genreDropdown, typeDropdown].forEach(dropdown => {
+        dropdown.addEventListener('click', () => {
+            const options = dropdown.querySelector('.dropdown-options');
+            options.classList.toggle('show');
+        });
+    });
+  
+    // Dropdown opciók kezelése
+    function handleDropdownSelection(dropdown, selectedElement, callback) {
+        const options = dropdown.querySelectorAll('li');
+        options.forEach(option => {
+            option.addEventListener('click', () => {
+                selectedElement.textContent = option.textContent;
+                callback();
+            });
+        });
+    }
+  
+    // Műfajok és típusok szűrése
+    handleDropdownSelection(genreDropdown, genreSelected, filterMovies);
+    handleDropdownSelection(typeDropdown, typeSelected, filterMovies);
+  
+    // Filmek szűrése
+    function filterMovies() {
+        const selectedGenre = genreSelected.textContent;
+        const selectedType = typeSelected.textContent;
+  
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            const genres = card.querySelector('.card_category').textContent.split(',').map(g => g.trim());
+            const type = card.querySelector('.card-type').textContent.trim();
+  
+            const genreMatch = selectedGenre === 'Összes' || genres.includes(selectedGenre);
+            const typeMatch = selectedType === 'Összes' || type.includes(selectedType);
+  
+            card.style.display = genreMatch && typeMatch ? 'block' : 'none';
+        });
+    }
+  
+    filterMovies();
+  });
+  // Hamburger menü toggle funkció
+  function toggleMenu() {
+      var nav = document.querySelector('nav');
+      nav.classList.toggle('open');  
+  }
